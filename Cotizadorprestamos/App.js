61506fa1b3bc3a2feb9cf3faac2c9bb0cd400344
ajimenezrosa <ja.jimenezrosa@gmail.com>
2021-02-15 15:08:18 -0400

    Crear Estructura

--- Cotizadorprestamos/App.js
+++ Cotizadorprestamos/App.js
@@ -5,12 +5,34 @@ import { StyleSheet, View, Text , SafeAreaView} from 'react-native';
 export default function App() {
   
   return(
-    <SafeAreaView>
-       <Text>Hola Alejandro</Text>
-    </SafeAreaView>
-  )
+
+    <>
+      <SafeAreaView style={styles.safeArea}>
+          <Text>Formulario HEAD</Text>
+      </SafeAreaView>
+
+      <View>
+          <Text>Resultado</Text> 
+      </View>
+      <View>
+        <Text>Footer</Text>
+      </View>
+    </>
+    );
 
 }
+
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#F00',
+    height:200,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    alignItems: 'center',
+  },
+});
+
 
 
 
--- Cotizadorprestamos/README.md
+++ Cotizadorprestamos/README.md
@@ -1,1 +1,32 @@
 # Cotizador-de-prestamos
+# 
+
+
+#### para este ejemplo eliminamos todo el codigo del archivo App.js
+
+#### estolo crearemos nosotros desde cero.
+
+#### podemos anadir el siguiente codigo.
+
+~~~javascript
+import React from 'react';
+import { StyleSheet, View, Text , SafeAreaView} from 'react-native';
+
+
+export default function App() {
+  
+  return(
+    <SafeAreaView>
+       <Text>Hola Alejandro</Text>
+    </SafeAreaView>
+  )
+
+}
+~~~
+
+primero Importamos el **'react-native'** para tener acceso a los elementos de React.
+
+luego importamos los elementos de ***'react-native'***, esto para tener acceso a los elemento a utilizar de React-native.
+
+
+
