b6a136268dab87afd5d8bffb0ce85624904bc6bc
ajimenezrosa <ja.jimenezrosa@gmail.com>
2021-02-15 17:16:39 -0400

    Formulario de Cotizacion

--- Cotizadorprestamos/src/components/Form.js
+++ Cotizadorprestamos/src/components/Form.js
@@ -4,10 +4,58 @@ import colors from '../utils/colors';
 
 export default function Form() {
     return(
-        <View>
+        <View style={styles.viewForm}>
+            <View style={styles.viewInputs}>
+
             <TextInput 
                 placeholder="Cantidad a pedir"
-            />
+                keyboardType='numeric'
+                style={styles.input}
+                />
+    
+            <TextInput 
+                placeholder="Interes %"
+                keyboardType='numeric'
+                style={[styles.input , styles.inputPercentage ]}
+                />
+           </View>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    viewForm: {
+        position: 'absolute',
+        bottom: -90,
+        width: '85%',
+        paddingHorizontal: 50,
+        backgroundColor: colors.PRIMARY_COLOR_DARK,
+        borderRadius: 30,
+        height: 180,
+        justifyContent: 'center',
+    },
+    viewInputs: {
+        flexDirection: 'row',
+
+    },
+    input: {
+        height: 50,
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: colors.PRIMARY_COLOR,
+        borderRadius: 5,
+        width: '60%',
+        marginRight: 5,
+        marginLeft: -5,
+        marginBottom: 10,
+        color: '#000',
+        paddingHorizontal: 20,
+    },
+    inputPercentage: {
+        width: '40%',
+        marginLeft: 5,
+    },
+
+
+});
